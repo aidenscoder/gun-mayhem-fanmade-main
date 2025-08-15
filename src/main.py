c3a53a2ecbcbb8e7117cdfc8fb4a9de3c45a0195
aidenscoder <amuah7109@icloud.com>
2025-08-15 08:48:08 -0400

    updated

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ drawing.load_img(r'src\assets\bg layer 2.png','bg-2',1.5,1.5)
 camera_x:drawing.number = 0
 camera_y:drawing.number = 0
 
-class positioner:
+class rect:
     def __init__(self,
         x:drawing.number,
         y:drawing.number,
@@ -24,9 +24,22 @@ class positioner:
     @property
     def conversion(self):
         return (self.x,self.y,self.size_x,self.size_y,self.rotation)
+    
+    @property
+    def conversion_NonRotation(self):
+        return (self.x,self.y,self.size_x,self.size_y)
+    
+    def conversion_offset(self,x:drawing.number,y:drawing.number):
+        return (self.x-x,self.y-y,self.size_x,self.size_y,self.rotation)
+    
+    def conversion_offset_NonRotation(self,x:drawing.number,y:drawing.number):
+        return (self.x-x,self.y-y,self.size_x,self.size_y)
 
-collisions = [(400,500,100,25)]
-def main(window:drawing.window):
+
+collisions = [rect(400,500,100,50,0)]
+my_rect = rect(400,300,50,50,0)
+
+def main(window:drawing.window,speed=[0,0]):
     global camera_x,camera_y 
     Mouse.update()
     def get_colliding(
@@ -40,16 +53,39 @@ def main(window:drawing.window):
         ):
             to_return = []
             for i in collisions:
-                to_return.append(drawing.collide_rect((*i,0),compare_rect))
+                to_return.append(drawing.collide_rect(i.conversion,compare_rect))
+                window.draw_rect(
+                    i.conversion_offset_NonRotation(camera_x,camera_y),
+                    rotation=i.rotation
+                )
             return to_return
             
-    
     camera_x = Mouse.x - window.width/2
     camera_y = Mouse.y - window.height/2
-    #window.draw_image((400-camera_x*0.05,500-camera_y*0.05),'bg-2')
-    #window.draw_image((400-camera_x*0.1,500-camera_y*0.1),'bg-1')
-    print(get_colliding(positioner(400,300,50,50,0).conversion))
-    window.draw_rect((400,300,50,50),drawing.ColorRgbA((255,0,0,150)),"Additive")
+    window.draw_image((400-camera_x*0.05,500-camera_y*0.05),'bg-2')
+    window.draw_image((400-camera_x*0.1,500-camera_y*0.1),'bg-1')
+    
+    current = get_colliding(my_rect.conversion)
+    
+    if Keyboard.is_pressed(Key.key_a,Key.key_LEFT):
+        speed[0] -= 5
+    if Keyboard.is_pressed(Key.key_d,Key.key_RIGHT):
+        speed[0] += 5
+    if Keyboard.is_pressed(Key.key_w,Key.key_UP):
+        speed[1] -= 5
+    if Keyboard.is_pressed(Key.key_s,Key.key_DOWN):
+        speed[1] += 5
+        
+    speed[0] *= 0.8
+    speed[1] *= 0.8
+    my_rect.x += speed[0]
+    my_rect.y += speed[1]
+    
+        
+    window.draw_rect(
+        my_rect.conversion_offset_NonRotation(camera_x,camera_y),
+        drawing.ColorRgbA((255,0,0,255))
+    )
     
 window.main_loop = main
 window.start(bg_color=drawing.ColorRgb((100,175,255)))
